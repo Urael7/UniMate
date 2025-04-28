b255542829ec8e8238989d31ef8b034376d2b3a8
Hermela Asmamaw <hermelaasemamaw@gmail.com>
2025-04-28 20:23:47 +0300

    Added User, Event, Task, Expense, Notice models

---
+++ backend/models/event.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    date: { type: Date, required: true },
+    location: { type: String },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', EventSchema);
